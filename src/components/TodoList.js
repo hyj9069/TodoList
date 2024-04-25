afcc9fce73f74af18860ebc5c0b465e497a4c665
han ye ji <hyj99811@gmail.com>
2024-04-25 14:35:14 +0900

    todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import TodoItem from './TodoItem'
+import React from "react";
+import TodoItem from "./TodoItem";
 
-function TodoList({todos, removeItem, toggleCompleted, editCompletedItem}) {
+function TodoList({ todos, removeItem, toggleCompleted, editCompletedItem }) {
   return (
     <div>
-      <ul className='todo_list'>
+      <ul className="todo_list">
         {todos.map((todo) => {
-          // <>쓸라면 return 넣어야됨
-          return <TodoItem key={todo.id} todo={todo} removeItem={removeItem} toggleCompleted={toggleCompleted} editCompletedItem={editCompletedItem}/>
+          // =>쓸라면 return 넣어야됨
+          return <TodoItem key={todo.id} todo={todo} removeItem={removeItem} toggleCompleted={toggleCompleted} editCompletedItem={editCompletedItem} />;
         })}
       </ul>
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
